d530f1abf1a763523898ed15b18c8c49037ae0fe
BilguunTs <usosketch@yahoo.com>
2021-03-21 15:20:37 +0800

    adding gradient background

--- lib/main.dart
+++ lib/main.dart
@@ -40,45 +40,25 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MySvgBG extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return SvgPicture.string(
-      '<svg viewBox="0.0 0.0 375.0 812.0" ><defs><linearGradient id="gradient" x1="1.0" y1="-0.006568" x2="0.0" y2="1.0"><stop offset="0.0" stop-color="#0088cbff" stop-opacity="0.0" /><stop offset="1.0" stop-color="#fffbe497"  /></linearGradient></defs><path  d="M 0 0 L 375 0 L 375 812 L 0 812 L 0 0 Z" fill="url(#gradient)" stroke="#707070" stroke-width="1" stroke-miterlimit="4" stroke-linecap="butt" /></svg>',
-      allowDrawingOutsideViewBox: true,
-    );
-  }
-}
-
-class WithSvgBackground extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Stack(alignment: Alignment.center, children: <Widget>[
-      Positioned(
-          child: Container(
-            alignment: Alignment.center,
-            child: MySvgBG(),
-            color: Colors.white70,
-            padding: const EdgeInsets.all(0),
-          ),
-          top: 0),
-      NFCButton()
-    ]);
-  }
-}
-
-class Test extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Text("Hi");
-  }
-}
-
 Route<dynamic> _onGenerateRoute(RouteSettings settings) {
   switch (settings.name) {
     case "/":
       return MaterialPageRoute(builder: (BuildContext context) {
-        return WithSvgBackground();
+        return Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                stops: [0.1, 0.5, 0.7, 0.9],
+                colors: [
+                  Colors.yellow[100],
+                  Colors.yellow[200],
+                  Colors.yellow[300],
+                  Colors.amber,
+                ],
+              ),
+            ),
+            child: NFCButton());
         //return Home();
       });
     case "/repair-flow":
